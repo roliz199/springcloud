ea15333604cf656133c6c20f9a5b4ce39d6b595f
PGAR <r3.perez@praxisglobal.net>
2024-09-09 20:02:41 -0600

    proyecto

---
+++ limits-services/src/test/java/com/curso/microservicios/limits_services/LimitsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.limits_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
